1129f0f5648adc11bfcfb7d5f7762537261ac482
Ethan Willingham <82549015+Ethan-Willingham@users.noreply.github.com>
2023-09-11 11:07:19 -0500

    Initail commit HTML only

--- assets/js/navigation.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("hamburger").addEventListener("click", function () {
-  const sidebar = document.querySelector(".sidebar");
-
-  if (sidebar.style.width === "0px" || sidebar.style.width === "") {
-    sidebar.style.width = "250px";
-  } else {
-    sidebar.style.width = "0px";
-  }
-});
